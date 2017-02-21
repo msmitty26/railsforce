8da5aeed94b16432d62ed145eeee46658d9bda58
Mark Smith <msmitty26@gmail.com>
2017-02-20 20:10:05 -0500

    added search methods to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,29 @@ class Contact < ApplicationRecord
 
   validates_presence_of :first_name, :last_name, :email_address, :organization
   validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "format is invalid" }
+  
+  def self.search_by_contact_full_name(simple_query)
+    where("CONCAT_WS(' ', first_name, last_name) LIKE :q", :q => "%#{simple_query}%")
+  end
+  
+  def find_by_first_name(first_name)
+    Contact.find(first_name)
+  end
+  
+  def find_by_last_name(last_name)
+    Contact.find(last_name)
+  end
+  
+  def find_by_email_address(email_address)
+    Contact.find(email_address)
+  end
+  
+  def find_by_organization(organization)
+    Contact.find(organization)
+  end
+  
+  def find_by_affiliation(affiliation)
+    Contact.where(:affiliation => affiliation)
+  end
+  
 end
