754d75ca4cf5cc25ebfdf05031c92303b21d4870
Mark Smith <msmitty26@gmail.com>
2017-02-20 20:14:48 -0500

    added new search route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  resources :contacts do
+    get 'search', on: :collection
+  end
+  
   root to: "contacts#index"
 end
