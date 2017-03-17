7cffd4f575fc80b7e96db0e7a42b6a430be2300b
Mark Smith <msmitty26@gmail.com>
2017-03-16 22:59:14 -0400

    added new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :affiliations
-  resources :contacts
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   resources :contacts do
     get 'search', on: :collection
   end
-  
+
+  resources :contacts
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
   root to: "contacts#index"
 end
